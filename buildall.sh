4f43a9bc8bbc92f3fc36e7a10418db54ea425a9d
HRH Prince Sven Olaf of CyberBunker-Kamphuis <cb3rob@xs4all.nl>
2021-11-25 23:31:03 +0000

    2021-11-25T23:31:03Z (THURSDAY)

--- buildall.sh
+++ buildall.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 gcc -Wall -O3 -o cb3rob-ax25-bbs cb3rob-ax25-bbs.c
 gcc -Wall -O3 -o cb3rob-ax25-bridge cb3rob-ax25-bridge.c
+gcc -Wall -O3 -o cb3rob-ax25-getty cb3rob-ax25-getty.c -lutil
 gcc -Wall -O3 -o cb3rob-kiss-tcp-attach cb3rob-kiss-tcp-attach.c -lutil
 gcc -Wall -O3 -o cb3rob-kiss-tcp-multiplexer cb3rob-kiss-tcp-multiplexer.c
