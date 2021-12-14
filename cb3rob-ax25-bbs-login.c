33e5c9e5e6880c35d2d602a843441970b6ee0c00
HRH Prince Sven Olaf of CyberBunker-Kamphuis <cb3rob@xs4all.nl>
2021-12-14 13:34:20 +0000

    2021-12-14T13:34:20Z (TUESDAY)

--- cb3rob-ax25-bbs-login.c
+++ cb3rob-ax25-bbs-login.c
@@ -563,14 +563,15 @@ size_t rsize;
 int parsefield;
 parsefield=0;
 sync();sleep(1);write(STDOUT_FILENO,"#NO#\r",5);sync();sleep(1);//DENY UPLOAD
-for(n=0;(bincmd[n]!=0)&&(bincmd[n]!='\r')&&(bincmd[n]!='#');n++){
-//if(bincmd[n]=='#'){
+for(n=0;(bincmd[n]!=0)&&(bincmd[n]!='\r');n++){
+if(bincmd[n]=='#'){
 n++;//SKIP FIELD DELIMITER ITSELF
 memset(&buf,0,sizeof(buf));
 //COPY FIELD TO BUF
-for(f=0;((n+f)<sizeof(buf)-1)&&(bincmd[n+f]!=0)&&(bincmd[n+f]!='\r')&&(bincmd[n+f]!='#');f++)buf[f]=bincmd[n+f];
+for(f=0;((n+f)<sizeof(buf)-1)&&(bincmd[n+f]!=0)&&(bincmd[n+f]!='\r')&&(bincmd([n+f]!='#');f++)buf[f]=bincmd[n+f];
 printf("FIELD: %d: [%s]\r",parsefield,buf);
 n=n+f;//FAST FORWARD N COUNTER TO NEXT DELIMITER
+n--;//PUT N BACK WHERE WE FOUND IT SO WE DON'T SKIP SEGMENTS
 parsefield++;
 //};//FOR FIELDCOPY
 };//FOR BYTE
