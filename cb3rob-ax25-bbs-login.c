c80c0e61d16a066c0530a4d9fb231ad93bb97946
HRH Prince Sven Olaf of CyberBunker-Kamphuis <cb3rob@xs4all.nl>
2021-12-14 13:27:21 +0000

    2021-12-14T13:27:21Z (TUESDAY)

--- cb3rob-ax25-bbs-login.c
+++ cb3rob-ax25-bbs-login.c
@@ -565,7 +565,7 @@ parsefield=0;
 sync();sleep(1);write(STDOUT_FILENO,"#NO#\r",5);sync();sleep(1);//DENY UPLOAD
 for(n=0;(bincmd[n]!=0)&&(bincmd[n]!='\r')&&(bincmd[n]!='#');n++){
 //if(bincmd[n]=='#'){
-//n++;//SKIP FIELD DELIMITER ITSELF
+n++;//SKIP FIELD DELIMITER ITSELF
 memset(&buf,0,sizeof(buf));
 //COPY FIELD TO BUF
 for(f=0;((n+f)<sizeof(buf)-1)&&(bincmd[n+f]!=0)&&(bincmd[n+f]!='\r')&&(bincmd[n+f]!='#');f++)buf[f]=bincmd[n+f];
